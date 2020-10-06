02a0d4f5fef4f85ef68acc76d5ecac8d79126525
marahmbawwab <marah.bawwab@gmail.com>
2020-10-06 19:38:41 +0300

    "dd"

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
+import { OverlayPanel } from 'primereact/overlaypanel';
 import about from './about.js';
 import welcome from './welcomep.js';
 
@@ -13,7 +14,8 @@ export default class App extends React.Component {
     this.state={open:false};
   this.handleclick=this.handleclick.bind(this);
   }
-  handleclick(){
+  handleclick(e){
+    this.op.toggle(e);
     this.setState(state => {
       return {
         open: !state.open,
@@ -26,20 +28,19 @@ export default class App extends React.Component {
      <Router>
     <div>
    <nav style={{ display:"flex",alignItems:"center",justifyContent:"flex-end",height:"60px",backgroundColor:"rgb(0,0,0,0.8)"}}>  
-     <img src={require('./list.png')}width="40px" height="40px"alt="there is an error" style={{marginRight:"50px"}} onClick={this.handleclick}></img>
+     <img  src={require('./list.png')}width="40px" height="40px"alt="there is an error" style={{marginRight:"50px"}} onClick={this.handleclick }></img>
+     <OverlayPanel ref={el => (this.op = el)}>
      {this.state.open && (
-            <div class="container">
               <ul style={{backgroundColor:"white"}}>
                 <li>Men's Fashions</li>
                 <li>Women's Fashions</li>
-                <li>Phones and Telecommunication</li>
                 <li>Home Items</li>
               </ul>
-            </div>
           )}
+          </OverlayPanel>
      <div style={{display:"flex",marginRight:"650px"}}>
    <p style={{color:"rgb(226,226,255)"}}><b>EASY</b></p>
-    <img src={require('./cart.png')} width="40px" height="40px" alt="there is an error"></img>
+    <img  src={require('./cart.png')} width="40px" height="40px" alt="there is an error" ></img>
     <p style={{color:"rgb(226,226,255)"}}><b>SHOP</b></p>
     </div>
    <Link to="/" style={{textDecoration:"none",color:"rgb(226,226,255)",marginRight:"10%"}}><b>Home</b></Link>
@@ -47,7 +48,7 @@ export default class App extends React.Component {
    </nav>
    <Switch>
      <Route  path ="/about" component={about}/>
-     <Route  path ="/" ><welcome/></Route>
+     <Route  path ="/" component={welcome}></Route>
    </Switch>
    </div>
 </Router>
