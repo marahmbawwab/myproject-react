2b25e66597f7c0b85eaa08bb07fdd5e073d594a9
marahmbawwab <marah.bawwab@gmail.com>
2020-10-27 11:06:31 +0200

    "ll"

--- my-app/src/server.js
+++ my-app/src/server.js
@@ -1,12 +1,16 @@
-var mysql = require('mysql');
-
-/*var con = mysql.createConnection({
-  host: "localhost",
-  user: "yourusername",
-  password: "yourpassword"
+const express=require('express');//create express server 
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+host: 'localhost',
+user:'root',
+password:'marah',//password of your mysql db
+database:'marah'
 });
 
 con.connect(function(err) {
   if (err) throw err;
-  console.log("Connected!");
-});*/
+  con.query("SELECT * FROM tasks", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
