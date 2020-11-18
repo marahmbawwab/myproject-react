60bb3fd05491da4e9c33fbe5bbe33d69f40fcdf5
marahmbawwab <marah.bawwab@gmail.com>
2020-11-18 22:24:19 +0200

    "AAA"

--- my-app/src/addproduct.js
+++ my-app/src/addproduct.js
@@ -4,58 +4,57 @@ import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 import { InputText } from 'primereact/inputtext';
-import {Password} from 'primereact/password';
-import { InputNumber } from 'primereact/inputnumber';
-import {Checkbox} from 'primereact/checkbox';
+import { Dropdown } from 'primereact/dropdown';
+import { InputTextarea } from 'primereact/inputtextarea';
 import {Button}  from 'primereact/button';
 export default class Add extends React.Component {
+    constructor(){
+        super();
+        this.state={name:"",describe:"",quan:"",cat:"",dep:"",size:""}
+    }
+    handle=()=>{
+   console.log(this.state);
+    }
 render(){
         return(
             <div className="d" >
             <div >
-            <div className="p-text-left dd"><b style={{fontSize:"35px",whiteSpace:"nowrap",marginLeft:"100%"}}>LET'S ADD PRODUCT</b></div>
+            <div className="p-text-left dd"><b style={{fontSize:"35px",whiteSpace:"nowrap"}}>LET'S ADD PRODUCT</b></div>
             <br/>
             <div  className="item">
-            <label htmlhtmlFor="full"><b style={{fontSize:"20px"}}>FullName:</b></label>
-              <InputText style={{marginLeft:"40%"}}  id="full" />
+            <label htmlhtmlFor="name"><b style={{fontSize:"20px"}}>ProductName:</b></label>
+              <InputText style={{marginLeft:"20%"}} id="name" onChange={(e)=>this.setState({name:e.target.value})} />
           </div>
           <br/>
           <div  className="item">
-            <label htmlhtmlFor="user"><b style={{fontSize:"20px"}}>UserName:</b></label>
-              <InputText style={{marginLeft:"35%"}} id="user" />
+            <label htmlhtmlFor="quantity"><b style={{fontSize:"20px"}}>Quantity:</b></label>
+              <InputText style={{marginLeft:"54%"}} id="quantity" onChange={(e)=>this.setState({quan:e.target.value})} />
           </div>
           <br/>
           <div  className="item" >
-            <label htmlhtmlFor="pass"><b style={{fontSize:"20px"}}>Password:</b></label>
-              <Password style={{marginLeft:"40%"}} id="pass" />
+            <label htmlhtmlFor="cat"><b style={{fontSize:"20px"}}>Category:</b></label>
+            <Dropdown style={{marginLeft:"40%",width:"300%"}} placeholder="Select a Category" onChange={(e)=>this.setState({cat:e.target.value})} />
           </div>
           <br/>
           <div  className="item" >
-            <label htmlhtmlFor="phone"><b style={{fontSize:"20px"}}>Phone:  </b>  </label>
-              <InputNumber style={{marginLeft:"65%"}} id="phone" />
+          <label htmlhtmlFor="dep"><b style={{fontSize:"20px"}}>Department:</b></label>
+            <Dropdown placeholder="Select a Department"style={{marginLeft:"35%",width:"200%"}} onChange={(e)=>this.setState({dep:e.target.value})}/>
           </div>
           <br/>
           <div className="item">
-            <label htmlhtmlFor="email"><b style={{fontSize:"20px"}}>Email:</b></label>
-              <InputText style={{marginLeft:"70%"}}id="email" keyfilter="Email" />
+          <label htmlhtmlFor="size"><b style={{fontSize:"20px"}}>Size:</b></label>
+            <Dropdown placeholder="Select a Size" style={{marginLeft:"45%",width:"300%"}} onChange={(e)=>this.setState({size:e.target.value})} />
           </div>
           <br/>
-          <div  style={{ marginLeft:"33%"}}>
-            <label htmlhtmlFor="gender"><b style={{fontSize:"20px"}}>Gender:</b></label>
-            <div style={{ marginLeft:"60%"}}>
-            <div className="p-col-12">
-    <Checkbox inputId="cb1" value="Male"></Checkbox>
-    <label htmlFor="cb1" className="p-checkbox-label"><b>Male</b></label>
-</div>
-<br/>
-<div className="p-col-12">
-    <Checkbox   inputId="cb2" value="Female"></Checkbox>
-    <label htmlFor="cb2" className="p-checkbox-label"><b>Female</b></label>
-</div>
-</div>
+          <div className="item">
+          <label htmlhtmlFor="desc"><b style={{fontSize:"20px",marginLeft:"50%"}}>Description:</b></label>
+          <div style={{display: "block"}} >
+          <InputTextarea style={{marginLeft:"30%"}} rows={4} cols={50} onChange={(e)=>this.setState({describe:e.target.value})} />
           </div>
-          <div style={{marginLeft:"80%",marginTop:"10%"}}>
-        <Button style={{width:"180px"}}className="p-button-help p-button-raised p-button-rounded" onClick={this.handle}><b style={{paddingRight:"20%",fontSize:"20px"}}>Register </b></Button>
+          </div>
+          <br/>
+          <div style={{marginLeft:"80%",marginTop:"2%"}}>
+        <Button style={{width:"180px"}}className=" p-button-raised p-button-rounded" onClick={this.handle}><b style={{paddingRight:"35%",fontSize:"20px"}}>ADD</b></Button>
             </div>
             </div>
             </div>
